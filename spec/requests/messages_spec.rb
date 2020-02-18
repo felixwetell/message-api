23d669214ebbc0f01a8426077f9dd550fcadde17
Felix Wetell <felix.wetell@gmail.com>
2020-02-19 00:18:18 +0100

    Updates message test to use correct routes

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -1,17 +1,13 @@
 RSpec.describe 'Messages API', type: :request do
-  # initialize test data 
   let!( :messages ) { create_list( :message, 10 ) }
   let( :message_id ) { messages.first.id }
 
-  # Test suite for GET /messages
-  describe 'GET /messages' do
-    # make HTTP get request before each example
-    before { get '/messages' }
+  describe 'GET /api/v1/messages' do
+    before { get '/api/v1/messages' }
 
     it 'returns messages' do
-      # Note json is a custom helper to parse JSON responses
-      expect( json ).not_to be_empty
-      expect( json.size ).to eq( 10 )
+      expect( JSON.parse( response.body ) ).not_to be_empty
+      expect( JSON.parse( response.body ).size ).to eq( 10 )
     end
 
     it 'returns status code 200' do
@@ -19,14 +15,13 @@ RSpec.describe 'Messages API', type: :request do
     end
   end
 
-  # Test suite for GET /messages/:id
-  describe 'GET /messages/:id' do
-    before { get "/messages/#{ message_id }" }
+  describe 'GET /api/v1/messages/:id' do
+    before { get "/api/v1/messages/#{ message_id }" }
 
     context 'when the message exists' do
       it 'returns the message' do
-        expect( json ).not_to be_empty
-        expect( json[ 'id' ] ).to eq( message_id )
+        expect( JSON.parse( response.body ) ).not_to be_empty
+        expect( JSON.parse( response.body )[ 'id' ] ).to eq( message_id )
       end
 
       it 'returns status code 200' do
@@ -42,21 +37,19 @@ RSpec.describe 'Messages API', type: :request do
       end
 
       it 'returns a not found message' do
-        expect( response.body ).to match( "Couldn't find message" )
+        expect( response.body ).to match( "Couldn't find Message with 'id'=#{ message_id }" )
       end
     end
   end
 
-  # Test suite for POST /messages
-  describe 'POST /messages' do
-    # valid payload
+  describe 'POST /api/v1/messages' do
     let( :valid_attributes ) { { title: 'New message', text: 'Saker som inte hänt', author: 'Felix Wetell' } }
 
     context 'when the request is valid' do
-      before { post '/messages', params: valid_attributes }
+      before { post '/api/v1/messages', params: valid_attributes }
 
       it 'creates a message' do
-        expect( json[ 'title' ] ).to eq( 'Learn Elm' )
+        expect( JSON.parse( response.body )[ 'title' ] ).to eq( 'New message' )
       end
 
       it 'returns status code 201' do
@@ -65,24 +58,23 @@ RSpec.describe 'Messages API', type: :request do
     end
 
     context 'when the request is invalid' do
-      before { post '/messages', params: { title: 'Foobar' } }
+      before { post '/api/v1/messages', params: { title: 'Foobar' } }
 
       it 'returns status code 422' do
         expect( response ).to have_http_status( 422 )
       end
 
       it 'returns a validation failure message' do
-        expect( response.body ).to match( "Validation failed: No fields can be blank" )
+        expect( response.body ).to match( "Validation failed: Text can't be blank, Author can't be blank" )
       end
     end
   end
 
-  # Test suite for PUT /messages/:id
-  describe 'PUT /messages/:id' do
+  describe 'PUT /api/v1/messages/:id' do
     let( :valid_attributes ) { { title: 'Old message' } }
 
     context 'when the message exists' do
-      before { put "/messages/#{ message_id }", params: valid_attributes }
+      before { put "/api/v1/messages/#{ message_id }", params: valid_attributes }
 
       it 'updates the message' do
         expect( response.body ).to be_empty
@@ -94,9 +86,8 @@ RSpec.describe 'Messages API', type: :request do
     end
   end
 
-  # Test suite for DELETE /messages/:id
-  describe 'DELETE /messages/:id' do
-    before { delete "/messages/#{ message_id }" }
+  describe 'DELETE /api/v1/messages/:id' do
+    before { delete "/api/v1/messages/#{ message_id }" }
 
     it 'returns status code 204' do
       expect( response ).to have_http_status( 204 )
