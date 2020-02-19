34d27a003f91ddcc5daa50c31fac23ac74694551
Felix Wetell <felix.wetell@gmail.com>
2020-02-19 11:55:47 +0100

    Updates user model logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   validates_presence_of :name
   validates_uniqueness_of :name, case_sensitive: false
   has_secure_password
+
+  before_save :downcase_name
+
+  def downcase_name
+    self.name = self.name.strip.downcase
+  end
 end
