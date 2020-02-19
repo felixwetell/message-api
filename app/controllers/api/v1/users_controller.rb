6743bfacfad954501f25844c302b5519f4d21811
Felix Wetell <felix.wetell@gmail.com>
2020-02-19 13:02:31 +0100

    Adds methods for user params and create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,20 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      def create
+        user = User.create!( user_params )
+        if user.save
+          json_response( user, :created )
+        else
+          json_response( user.errors.full_messages, :unprocessable_entity )
+        end
+      end
 
+      private
+
+      def user_params
+        params.require( :user ).permit( :name, :password_digest )
+      end
     end
   end
 end
