c4d22ecd248b1d9b3883885a5f39e34805e69099
Felix Wetell <felix.wetell@gmail.com>
2020-02-19 13:03:08 +0100

    Forces model type

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :message do
+  factory :message, class: Message do
     title { Faker::Games::ElderScrolls.race }
     text { Faker::Lorem.paragraph }
     author { Faker::Games::ElderScrolls.dragon }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :user do
+  factory :user, class: User do
     name { Faker::Games::ElderScrolls.name }
     password { Faker::Company.buzzword }
   end
