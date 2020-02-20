7a59630220198c53414c879f74080487f06f9f63
Felix Wetell <felix.wetell@gmail.com>
2020-02-20 16:05:55 +0100

    Adds authentication logic

---
+++ app/auth/authenticate_user.rb
@@ -0,0 +1,23 @@
+require 'notice'
+
+class AuthenticateUser
+  def initialize( name, password )
+    @name = name
+    @password = password
+  end
+
+  def call
+    JsonWebToken.encode( user_id: user.id ) if user
+  end
+
+  private
+
+  attr_reader :name, :password
+
+  def user
+    user = User.find_by( name: name )
+    return user if user && user.authenticate( password )
+
+    raise( ExceptionHandler::AuthenticationError, Notice.invalid_credentials )
+  end
+end
---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,12 @@
+class AuthenticationController < ApplicationController
+  def autenticate
+    auth_token = AuthenticateUser.new( auth_params[ :name ], auth_params[ :password ] ).call
+    json_response( auth_token: auth_token )
+  end
+
+  private
+
+  def auth_params
+    params.permit( :name, :password )
+  end
+end
