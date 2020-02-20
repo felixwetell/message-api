741484a890e5f46693ff8fb5b23978c5f2dc5411
Felix Wetell <felix.wetell@gmail.com>
2020-02-20 16:21:17 +0100

    Removes resources user controller route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :messages
-      resources :users, only: :create
       post 'auth/login', to: 'authentication#authenticate'
       post 'signup', to: 'users#create'
     end
