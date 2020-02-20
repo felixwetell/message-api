e70cdde2af55a85b2cf95fadc4cbed2bb73f3a32
Felix Wetell <felix.wetell@gmail.com>
2020-02-20 20:58:29 +0100

    Corrects routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'api/v1/users#create'
   namespace 'api' do
     namespace 'v1' do
       resources :messages
-      post 'auth/login', to: 'authentication#authenticate'
-      post 'signup', to: 'users#create'
     end
   end
 end
