9b1435eb9af313ed1c15c278b83b0c17e2629dd9
Felix Wetell <felix.wetell@gmail.com>
2020-02-20 16:00:13 +0100

    Update message_spec.rb

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Message, type: :model do
   it { should validate_presence_of( :title ) }
   it { should validate_presence_of( :text ) }
   it { should validate_presence_of( :author ) }
+  it { should validate_presence_of( :user_id ) }
 end
