dea4ab554a4b8767641639461b221492243012e4
Felix Wetell <felix.wetell@gmail.com>
2020-02-20 16:06:51 +0100

    Adds singleton called notice

---
+++ lib/notice.rb
@@ -0,0 +1,34 @@
+class Notice
+  def self.not_found( record = 'record' )
+    "Sorry, #{record} was not found."
+  end
+
+  def self.invalid_credentials
+    'Invalid credentials'
+  end
+
+  def self.invalid_token
+    'Invalid token'
+  end
+
+  def self.missing_token
+    'Missing token'
+  end
+
+  def self.unauthorized
+    'Unauthorized request'
+  end
+
+  def self.account_created
+    'User created successfully'
+  end
+
+  def self.account_not_created
+    'User could not be created'
+  end
+
+  def self.expired_token
+    'Sorry, token has expired. Please login to continue.'
+  end
+
+end
