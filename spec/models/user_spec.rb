f1ea79d9773e0623400998232a01f386961223a5
Felix Wetell <felix.wetell@gmail.com>
2020-02-20 15:59:57 +0100

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  it { should have_many :messages }
   it { should validate_presence_of( :password ) }
   it { should validate_presence_of( :name ) }
 
-  # Shoulda matchers can not validate uniqueness of column if record is empty
+  # Shoulda matchers can not validate uniqueness of column if record is empty, see source below
   # https://www.rubydoc.info/github/thoughtbot/shoulda-matchers/Shoulda%2FMatchers%2FActiveRecord%3Avalidate_uniqueness_of
   describe 'users name should be unique' do
     subject { FactoryBot.create( :user ) }
