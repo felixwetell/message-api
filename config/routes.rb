08fa51cdc008ac3b7914bcdb5e030f0429bd482e
Felix Wetell <felix.wetell@gmail.com>
2020-02-23 17:57:56 +0100

    Updated route to match signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post 'auth/login', to: 'authentication#authenticate'
+  post 'login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
   namespace 'api' do
     namespace 'v1' do
